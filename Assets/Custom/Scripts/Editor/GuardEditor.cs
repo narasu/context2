11a7b2e1b822c085f91f5c637caddde800dba759
Michael Yamada <roninnarasu@gmail.com>
2024-03-18 20:33:03 +0100

    node selection improvements

--- Assets/Custom/Scripts/Editor/GuardEditor.cs
+++ Assets/Custom/Scripts/Editor/GuardEditor.cs
@@ -11,7 +11,8 @@ public class GuardEditor : Editor
     
     private Guard guard;
     private SerializedProperty pathNodesList;
-    private int selectedNodeIndex;
+    private int selectedNode;
+    private int newNodeIndex = -1;
     
     private Dictionary<SerializedProperty, int> pathControls = new();
 
@@ -46,92 +47,125 @@ public class GuardEditor : Editor
         
         if (GUILayout.Button(buttonString))
         {
+            newNodeIndex = pathNodesList.arraySize;
             Vector3 initPos;
             Quaternion initRot;
 
             if (guard.PathNodes.Count == 0)
             {
                 initRot = guard.transform.rotation;
-                initPos = initRot * Vector3.forward;
+                initPos = initRot * Vector3.forward * 2.0f;
             }
             else
             {
-                initPos = guard.PathNodes[^1].Position + guard.PathNodes[^1].Forward;
-                initRot = guard.PathNodes[^1].Rotation;
+                var pn = pathNodesList.GetArrayElementAtIndex(guard.PathNodes.Count - 1);
+                
+                Vector3 p = pn.FindPropertyRelative("Position").vector3Value;
+                Quaternion r = pn.FindPropertyRelative("Rotation").quaternionValue;
+                
+                initPos = p + r * Vector3.forward;
+                initRot = r;
+                
             }
-            
-            guard.PathNodes.Add(new PathNode(initPos, initRot));
-            Utility.RefreshSceneView();
+
+            pathNodesList.InsertArrayElementAtIndex(pathNodesList.arraySize);
+            SerializedProperty n = pathNodesList.GetArrayElementAtIndex(pathNodesList.arraySize - 1);
+            n.FindPropertyRelative("Position").vector3Value = initPos;
+            n.FindPropertyRelative("Rotation").quaternionValue = initRot;
+            newNodeIndex = -1;
         }
 
         serializedObject.ApplyModifiedProperties();
-        // if (serializedObject.ApplyModifiedProperties())
-        // {
-        //     if (serializedObject.hasModifiedProperties)
-        //     {
-        //         // if a new node was added, generate a controlID and add it to the dictionary
-        //         if (pathNodesList.arraySize > pathControls.Count)
-        //         {
-        //             for (int i = 0; i < pathNodesList.arraySize; i++)
-        //             {
-        //                 pathControls.TryAdd(pathNodesList.GetArrayElementAtIndex(i), GUIUtility.GetControlID(FocusType.Passive));
-        //             }
-        //         }
-        //         
-        //         // if a node was removed, remove it from the dictionary
-        //         else if (pathNodesList.arraySize < pathControls.Count)
-        //         {
-        //             for (int i = 0; i < pathNodesList.arraySize; i++)
-        //             {
-        //                 SerializedProperty pathNode = pathNodesList.GetArrayElementAtIndex(i);
-        //                 int controlID = pathControls.FirstOrDefault(_x => _x.Key.Equals(pathNode)).Value;
-        //                 if (controlID == 0)
-        //                 {
-        //                     pathControls.Remove(pathNode);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+        Utility.RefreshSceneView();
     }
 
     private void OnSceneGUI()
     {
         serializedObject.Update();
-        if (guard.PathNodes.Count == 0)
+        if (pathNodesList.arraySize == 0)
         {
             return;
         }
-
+        
         Vector3 guardPos = guard.transform.position;
+
+        if (newNodeIndex >= 0)
+        {
+            
+        }
         
         for(int i=0; i< pathNodesList.arraySize; i++)
         {
             SerializedProperty pathNode = pathNodesList.GetArrayElementAtIndex(i);
             
-            
+            // get position and rotation to draw node at
             Vector3 pos = guardPos + pathNode.FindPropertyRelative("Position").vector3Value;
             Quaternion rot = pathNode.FindPropertyRelative("Rotation").quaternionValue;
 
-            
+            // draw node
             Handles.color = Color.blue;
             int controlID = GUIUtility.GetControlID(FocusType.Passive);
-            Handles.SphereHandleCap(controlID, pos, rot, .5f, EventType.Repaint);
-            if (Event.current.GetTypeForControl(controlID) == EventType.MouseDown && HandleUtility.nearestControl == controlID)
+            Handles.SphereHandleCap(controlID, pos, rot, .25f, EventType.Repaint);
+            
+            // if node is selected, draw transform gizmo
+            if (selectedNode == controlID)
             {
-                Debug.Log("Gizmo selected!");
-                // pos = Handles.PositionHandle(pos, rot);
-                // pathNode.FindPropertyRelative("Position").vector3Value = pos - guardPos;
-                //
-                // rot = Handles.RotationHandle(rot, pos);
-                // pathNode.FindPropertyRelative("Rotation").quaternionValue = rot;
-                Event.current.Use();
+                EnableTransformHandles(pathNode, pos, rot);
             }
-            if (Event.current.GetTypeForControl(controlID) == EventType.Layout)
+            // otherwise, just draw a line indicating its forward direction
+            else
             {
-                HandleUtility.AddDefaultControl(controlID);
+                Handles.DrawAAPolyLine(4.0f, pos + Vector3.up * 0.1f, pos + rot * Vector3.forward * 0.5f + Vector3.up * 0.1f);
+            }
+
+            var evt = Event.current;
+            switch (evt.GetTypeForControl(controlID))
+            {
+                case EventType.Layout:
+                    if (selectedNode != controlID)
+                    {
+                        float distanceToHandle = HandleUtility.DistanceToCircle(pos, .25f);
+                        HandleUtility.AddControl(controlID, distanceToHandle);
+                    }
+                    break;
+                case EventType.MouseDown:
+                    if (HandleUtility.nearestControl == controlID && evt.button == 0)
+                    {
+                        if (selectedNode != controlID)
+                        {
+                            Debug.Log("Gizmo selected!");
+                            GUIUtility.hotControl = controlID;
+                            selectedNode = controlID;
+                            evt.Use();
+                        }
+                    }
+                    break;
             }
             serializedObject.ApplyModifiedProperties();
         }
+    }
+
+    private void DrawNode(SerializedProperty _pathNode)
+    {
+        Vector3 guardPos = guard.transform.position;
+        // get position and rotation to draw node at
+        Vector3 pos = guardPos + _pathNode.FindPropertyRelative("Position").vector3Value;
+        Quaternion rot = _pathNode.FindPropertyRelative("Rotation").quaternionValue;
+
+        // draw node
+        Handles.color = Color.blue;
+        int controlID = GUIUtility.GetControlID(FocusType.Passive);
+        Handles.SphereHandleCap(controlID, pos, rot, .25f, EventType.Repaint);
+    }
+
+    private void EnableTransformHandles(SerializedProperty _pathNode, Vector3 _pos, Quaternion _rot)
+    {
+        Vector3 guardPos = guard.transform.position;
+        
+        _pos = Handles.PositionHandle(_pos, _rot);
+        _pathNode.FindPropertyRelative("Position").vector3Value = _pos - guardPos;
+                
+        _rot = Handles.RotationHandle(_rot, _pos);
+        _pathNode.FindPropertyRelative("Rotation").quaternionValue = _rot;
     }
 }
