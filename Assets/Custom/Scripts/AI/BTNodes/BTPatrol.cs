f8ef18c319832cb87cd2d2193d2e6a7e3c306943
Michael Yamada <roninnarasu@gmail.com>
2024-03-24 20:17:25 +0100

    added BTCounter

---
+++ Assets/Custom/Scripts/AI/BTNodes/BTPatrol.cs
@@ -0,0 +1,13 @@
+﻿public class BTPatrol : BTComposite
+{
+    private Blackboard blackboard;
+    public BTPatrol(BTMoveTo _moveTo, Blackboard _blackboard) : base("Patrol", _moveTo, new BTLookAround(_blackboard))
+    {
+        blackboard = _blackboard;
+        
+    }
+    protected override TaskStatus Run()
+    {
+        return TaskStatus.Running;
+    }
+}
--- Assets/Custom/Scripts/_General/Strings.cs
+++ Assets/Custom/Scripts/_General/Strings.cs
@@ -4,13 +4,12 @@
     public static string Player = "Player";
     public static string Agent = "Agent";
     public static string Animator = "Animator";
-    public static string WeaponCrates = "WeaponCrates";
     public static string PatrolNodes = "PatrolNodes";
+    public static string PatrolPosition = "PatrolPosition";
     public static string Target = "Target";
     public static string DetectionResult = "DetectionResult";
     public static string ViewCone = "ViewCone";
     public static string ViewTransform = "ViewTransform";
     public static string StartingPosition = "StartingPosition";
-    public static string InputAsset = "InputAsset";
     public static string InputManager = "InputManager";
 }
