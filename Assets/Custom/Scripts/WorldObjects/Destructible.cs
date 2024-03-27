d4bcd6c3b6e081ec4ed614bd524e6958adf6ae7b
VuurGirl <vuurgirl.karlijn.snijders@gmail.com>
2024-03-27 21:14:42 +0100

    VFX destroing machien +sound fixes

--- Assets/Custom/Scripts/WorldObjects/Destructible.cs
+++ Assets/Custom/Scripts/WorldObjects/Destructible.cs
@@ -5,11 +5,13 @@ using UnityEngine.Events;
 
 public class Destructible : MonoBehaviour, IDamageable
 {
+    public GameObject Explosion;
     public UnityEvent OnTakeDamage;
     public void TakeDamage()
     {
         OnTakeDamage?.Invoke();
         ScoreManager.Instance.MachineCounter--;
+        Instantiate(Explosion,transform.position,new Quaternion(0,90,0,0));
         Destroy(gameObject);
     }
 }
